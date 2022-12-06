ac4869a2f5ae9ded110f7ebdbb147d8f4866de11
tryaspane <trypane98@gmail.com>
2022-12-06 21:32:37 +0700

    add new try-reporting-branch

--- src/TestRunner/Runner.java
+++ src/TestRunner/Runner.java
@@ -5,6 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Features", glue = {"StepDefinition"}, plugin = { "pretty", "html:target/cucumber-reports" })
+@CucumberOptions(
+        features = "Features",
+        glue = {"StepDefinition"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"})
 public class Runner {
 }
