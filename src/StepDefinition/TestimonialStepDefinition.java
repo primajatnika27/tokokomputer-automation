f42ff1caf84231edba070752d633703a4f1e6915
Prima Jatnika <prima.jatnika@majoo.id>
2022-12-16 21:32:38 +0700

    Testimonial

---
+++ src/StepDefinition/TestimonialStepDefinition.java
@@ -0,0 +1,22 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestimonialStepDefinition {
+    @Given("user navigate to application URL testimonial")
+    public void userNavigateToApplicationURLTestimonial() {
+    }
+
+    @When("user input name {string} email {string} title {string} testimonial {string}")
+    public void userInputNameEmailTitleTestimonial(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @And("user click on submit button")
+    public void userClickOnSubmitButton() {
+    }
+
+    @Then("testimonial created")
+    public void testimonialCreated() {
+    }
+}
